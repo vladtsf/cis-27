85d4080d3f136e74dade2754b53d9340c09a3d38
Vlad Tsvang <vtsvang@gmail.com>
2016-03-04 23:04:02 -0800

    Finalizing the assignment

--- lab1/Lab1/src/lab1/ArithmeticExpressions/ArithmeticExpressionEvaluator.java
+++ lab1/Lab1/src/lab1/ArithmeticExpressions/ArithmeticExpressionEvaluator.java
@@ -1,5 +1,17 @@
 package lab1.ArithmeticExpressions;
 
 public class ArithmeticExpressionEvaluator {
+    private final PostfixExpression postfix;
+    private final InfixExpression infix;
     
+    public ArithmeticExpressionEvaluator(String expression) {
+        infix = new InfixExpression(expression);
+        postfix = new PostfixExpression(infix);
+    }
+    
+    public void evaluate() {
+        System.out.format("Infix: %s\n", infix.toString());
+        System.out.format("Postfix: %s\n", postfix.toString());
+        System.out.format("Result: %.2f%n\n", postfix.evaluate());
+    }
 }
--- lab1/Lab1/src/lab1/Lab1.java
+++ lab1/Lab1/src/lab1/Lab1.java
@@ -1,39 +1,35 @@
 package lab1;
 
-import lab1.ArithmeticExpressions.InfixExpression;
-import lab1.ArithmeticExpressions.PostfixExpression;
+import lab1.ArithmeticExpressions.*;
 import static lab1.LinkedList.DoubleNode;
 
 public class Lab1 {
 
     public static void main(String[] args) {
+        
+        // 1. Linked Lists - Dequeue
         LinkedList ll = new LinkedList();
         
         DoubleNode first = new DoubleNode("first");
         DoubleNode last = new DoubleNode("last");
         
-//        ll.insertAtBeginning(first);
-//        ll.insertAtEnd(last);
-//        System.out.println("1: " + ll);
-//        ll.insertBefore(new LinkedList.DoubleNode("beforeLast"), last);
-//        ll.insertAfter(new LinkedList.DoubleNode("afterFirst"), first);
-//        System.out.println("2: " + ll);
-//        ll.moveToFront(last);
-//        ll.moveToEnd(first);
-//        System.out.println("3: " + ll);
-//        ll.remove(first);
-//        ll.removeFromBeginning();
-//        ll.removeFromEnd();
-//        System.out.println("4: " + ll);
+        ll.insertAtBeginning(first);
+        ll.insertAtEnd(last);
+        System.out.println("1: " + ll);
+        ll.insertBefore(new LinkedList.DoubleNode("beforeLast"), last);
+        ll.insertAfter(new LinkedList.DoubleNode("afterFirst"), first);
+        System.out.println("2: " + ll);
+        ll.moveToFront(last);
+        ll.moveToEnd(first);
+        System.out.println("3: " + ll);
+        ll.remove(first);
+        ll.removeFromBeginning();
+        ll.removeFromEnd();
+        System.out.println("4: " + ll);
 
-        // 300 23 + 43 21 - * 84 7 + /
-        System.out.println(new PostfixExpression(new InfixExpression("(300+23)*(43-21)/(84+7)")));
-        
-        // 4 8 + 6 5 - * 3 2 - 2 2 + * /
-        System.out.println(new PostfixExpression(new InfixExpression("(4+8)*(6-5)/((3-2)*(2+2))"))); 
-        
-        // 3
-        System.out.println(new InfixExpression("(4+8)*(6-5)/((3-2)*(2+2))").evaluate());
+        // 2. Stacks - Evaluating Arithmetic Expressions        
+        new ArithmeticExpressionEvaluator("(300+23)*(43-21)/(84+7)").evaluate();
+        new ArithmeticExpressionEvaluator("(4+8)*(6-5)/((3-2)*(2+2))").evaluate();
     }
     
 }
